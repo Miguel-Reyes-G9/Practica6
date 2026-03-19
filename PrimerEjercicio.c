36cf7ef0195ee9d36abc747c498182ff050836ed
Miguel-Reyes-G9 <miguel.alberto.reyes.morales@gmail.com>
2026-03-19 14:17:30 -0600

    Add files via upload

---
+++ PrimerEjercicio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+// Comentario por línea
+/* Comentario por bloque
+    que puede ocupar
+    varios renglones */
+// Este código compila y ejecuta
+/* pero no muestra salida alguna
+    debido a que un comentario
+    ya sea por línea o por bloque */
+// no es tomado en cuenta al momento
+// de compilar el programa,
+/* sólo sirve como documentación en el */ 
+/*
+    código fuente
+*/
+return 0;
+}
